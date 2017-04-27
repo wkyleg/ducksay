429d38e46a36cb553a87eef2c18ff6390951ea04
wkyleg <williamkylegraves@gmail.com>
2017-04-26 23:46:51 -0400

    Added js files

---
+++ index.js
@@ -0,0 +1,70 @@
+"use strict";
+
+const {
+  Requester
+} = require("node-duckduckgo");
+const requester = new Requester("ducksay");
+const Box = require("cli-box");
+
+// If no arguments are supplied, exit and display suggested use
+if (process.argv.length <= 2) {
+  console.log("Usage: " + __filename +
+    " <whatever you would like to ask the duck>");
+  process.exit(-1);
+}
+
+// Create string for requester from comand line arguments
+let requestString = '';
+for (let i = 2; i < process.argv.length; i++) {
+  requestString += process.argv[i] + ' ';
+}
+console.log(requestString);
+
+// Submit request to DuckDuckGo Instant, then handle results
+requester.request(requestString, (err, response, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  // Assign "AbstractText" to answer. If empty/null assign to error message
+  let answer = JSON.parse(body)["AbstractText"];
+  if (answer === null || answer === '') {
+    answer = "Sorry, don't know anything about that!"
+  }
+
+  // create small message box which will expand with size of reponce
+  const messageBox = Box("2x2", {
+    text: answer,
+    stretch: true,
+    autoEOL: true,
+    vAlign: "top",
+    hAlign: "left"
+  });
+
+  // Print reponce, then duck
+  console.log(messageBox);
+  console.log(duckImage);
+});
+
+
+// ANCI art of duck to print
+const duckImage =
+  `
+            \\
+             \\
+              \\
+               \\             ▒▒▒▒▒
+                \\         ▒▒▒▒▒░░▒▒
+                 \\      ▓▒ ░░░░░░░▒
+                     ▓▓▓▒▒▓▒▒▒▒░░░░▒▒
+                       ▓▓▓▓▓▒▒▒░░░▒▒           ▒
+                           ▓▒▒▒▒▒▒▒          ▓▓▒
+                          ▒▒▒▒▒▒▒░▒▒▒▒▒▒▒▒▒▒▒▒▒░
+                         ▒▒▒▒▒░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+                        ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▒▒▒░▒
+                       ▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░▒▒
+                       ▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+                        ▓▒▒▒▒▒▒▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+                           ▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+`;
